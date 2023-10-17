83c50b5bbd3907b6f106dcb44df05604598542c3
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2023-10-17 16:28:48 -0300

    add submenu
    submenu Store and Mac

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("JS")
+// const itensMenu = document.querySelector(".itens-menu");
+
+// itensMenu.addEventListener("mouseover", (event) => {
+
+//    let submenu = document.querySelector(".nav-list-dropdown")
+// if(submenu.contains("dropdown")){
+//   console.log("hummm")
+// }
+  
+//   console.log()
+
+//   console.log("aqui")
+// });
+
+// itensMenu.onmouseover = (event) => {
+//   console.log("ali")
+// };
